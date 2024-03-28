28fd6a06689ec248354379f84d90eee5d3a67d46
renato <renato.poslovni@gmail.com>
2024-03-28 15:28:25 +0100

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import NaiveRouter from "./components/NaiveRouter";
+import Navigation from "./components/Navigation";
+
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <Navigation />
+      <NaiveRouter />
+      <p>initial commit</p>
+    </div>
+  );
+}
+
+export default App;
